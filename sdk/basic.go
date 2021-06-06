b74d110ce9cc7e5e66700b4baceaa8d1bf93adeb
Yang Luo <hsluoyz@qq.com>
2021-06-06 21:17:00 +0800

    Rename to auth package.

--- sdk/basic.go
+++
@@ -1,8 +0,0 @@
-package sdk
-
-type Response struct {
-	Status string      `json:"status"`
-	Msg    string      `json:"msg"`
-	Data   interface{} `json:"data"`
-	Data2  interface{} `json:"data2"`
-}
