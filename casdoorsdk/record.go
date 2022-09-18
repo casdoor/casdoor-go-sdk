ba785de74909b4cfa6121c4a35ded0187c2e4e7e
Gucheng Wang <nomeguy@qq.com>
2022-09-18 20:51:11 +0800

    fix: add AddRecord API

---
+++ casdoorsdk/record.go
@@ -0,0 +1,57 @@
+// Copyright 2022 The Casdoor Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing records and
+// limitations under the License.
+
+package casdoorsdk
+
+import "encoding/json"
+
+type Record struct {
+	Id int `xorm:"int notnull pk autoincr" json:"id"`
+
+	Owner       string `xorm:"varchar(100) index" json:"owner"`
+	Name        string `xorm:"varchar(100) index" json:"name"`
+	CreatedTime string `xorm:"varchar(100)" json:"createdTime"`
+
+	Organization string `xorm:"varchar(100)" json:"organization"`
+	ClientIp     string `xorm:"varchar(100)" json:"clientIp"`
+	User         string `xorm:"varchar(100)" json:"user"`
+	Method       string `xorm:"varchar(100)" json:"method"`
+	RequestUri   string `xorm:"varchar(1000)" json:"requestUri"`
+	Action       string `xorm:"varchar(1000)" json:"action"`
+
+	ExtendedUser *User `xorm:"-" json:"extendedUser"`
+
+	IsTriggered bool `json:"isTriggered"`
+}
+
+func AddRecord(record *Record) (bool, error) {
+	if record.Owner == "" {
+		record.Owner = authConfig.OrganizationName
+	}
+	if record.Organization == "" {
+		record.Organization = authConfig.OrganizationName
+	}
+
+	postBytes, err := json.Marshal(record)
+	if err != nil {
+		return false, err
+	}
+
+	resp, err := DoPost("add-record", nil, postBytes, false, false)
+	if err != nil {
+		return false, err
+	}
+
+	return resp.Data == "Affected", nil
+}
